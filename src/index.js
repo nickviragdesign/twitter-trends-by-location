37c72fa62b0d0f3fd34e033b697e49807cf040c5
Nicholas Virag <nickviragmusic@gmail.com>
2018-02-05 22:48:15 -0500

    Changed return object to match the resolved promise

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-require('babel-polyfill');
-const rp = require('request-promise');
-const nodeGeocoder = require('node-geocoder');
+require('babel-polyfill')
+const rp = require('request-promise')
+const nodeGeocoder = require('node-geocoder')
 const optionsGeocode = {
 	provider: 'google',
 	httpAdapter: 'https',
 	apiKey: process.env.GOOGLE_API,
 	formatter: null
-};
-const geocoder = nodeGeocoder(optionsGeocode);
+}
+const geocoder = nodeGeocoder(optionsGeocode)
 
 exports.coordinates = async function(location) {
 	try {
 		const getCoords = await geocoder.geocode(location)
 		const coords = {
-			latitude: res[0].latitude,
-			longitude: res[0].longitude
+			latitude: getCoords[0].latitude,
+			longitude: getCoords[0].longitude
 		}
 		return coords
 	} catch(e) {
