fe18ee70a7a30e3dc348351701c06b3e4a511131
Nicholas Virag <nickviragmusic@gmail.com>
2018-07-10 20:28:57 -0400

    Added two more methods for getting Twitter trending locations and topics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('babel-polyfill')
+
 const rp = require('request-promise')
 const nodeGeocoder = require('node-geocoder')
 const optionsGeocode = {
@@ -9,15 +10,57 @@ const optionsGeocode = {
 }
 const geocoder = nodeGeocoder(optionsGeocode)
 
-exports.coordinates = async function(location) {
-	try {
-		const getCoords = await geocoder.geocode(location)
-		const coords = {
-			latitude: getCoords[0].latitude,
-			longitude: getCoords[0].longitude
-		}
-		return coords
-	} catch(e) {
-		throw Error(e)
+exports.coordinates = location => {
+	const getCoords = geocoder.geocode(location)
+	const coords = {
+		latitude: getCoords[0].latitude,
+		longitude: getCoords[0].longitude
 	}
+	return coords
+}
+
+exports.closestAreaWithTrends = (latitude, longitude) => {
+	const optionsWoeid = {
+		url: `https://api.twitter.com/1.1/trends/closest.json?lat=${latitude}&long=${longitude}`,
+		headers: {
+			'Authorization': process.env.TWITTER_BEARER,
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'mode': 'cors'}
+	}
+
+	return rp.get(optionsWoeid, (error, response, body) => {
+		if (error) {
+			throw error
+		} else {
+			return JSON.parse(response.body)[0]
+		}
+	})
+}
+
+//
+// Left Off
+//
+
+exports.getTrendingTopics = closestTrending => {
+
+    const woeid = JSON.parse(closestTrending)[0].woeid
+    const topicsURL = 'https://api.twitter.com/1.1/trends/place.json?id=' + woeid
+
+    const optionsTopics = {
+        url: topicsURL,
+        headers: {
+            'Authorization': process.env.TWITTER_BEARER,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'mode': 'cors'}
+    }
+
+    return rp.get(optionsTopics, (error, response, body) => {
+        if (error) {
+            throw error
+        } else {
+            return response.body
+        }
+    })
 }
